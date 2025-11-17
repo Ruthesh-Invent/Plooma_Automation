f64f37401cbae1725c211cbc07ccbbe63fcb6401
Ruthesh <rutheshkumarg@inventsoftlabs.com>
2025-11-18 00:05:33 +0530

    Initial Push

--- src/main/java/org/framework/playwright/constants/FrameWorkConstants.java
+++ src/main/java/org/framework/playwright/constants/FrameWorkConstants.java
@@ -10,10 +10,7 @@ public class FrameWorkConstants {
     public static final String SCREENSHOT_PATH_PREFIX = System.getProperty("user.dir") + "/Screenshot";
     public static final String SCREENSHOT_PATH_SUFFIX = ".png";
     public static int TIME_OUT_SECONDS = 10;
-    public static String APP_URL = "https://bmalpha.centralindia.cloudapp.azure.com/";
-    public static final String PATH_NAME = "accessToken.txt";
-    public static String PORTAL_URL = "https://qa.botminds.ai/";
-    public static String QAENV_URL ="https://qa.botminds.ai/";
-    public static String DEVENV_URL ="https://qa.botminds.ai/";
+    public static String QAENV_URL ="http://pmsuatadmin.inventsoftlabs.com/authentication/signin";
+    
 
 }
