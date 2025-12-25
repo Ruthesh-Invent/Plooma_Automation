603bcf6d9317b68b3f409cf77ae0ddc32a863a82
Ruthesh <rutheshkumarg@inventsoftlabs.com>
2025-12-25 14:37:46 +0530

    Encounter form test script

--- src/main/java/org/playwright/pages/AppointmentPage.java
+++ src/main/java/org/playwright/pages/AppointmentPage.java
@@ -26,31 +26,32 @@ public class AppointmentPage extends BaseClass {
 //		click(submoduleElement, subModuleName);	
 //		 System.out.println("Clicked on the subModuleName ");
 //	}
-	
+
 	public void clickAddapointment() {
-	    String xpath = "//mat-icon[normalize-space()='add']";
-	    Locator element = getPage().locator(xpath);
-	    element.waitFor(new Locator.WaitForOptions().setState(WaitForSelectorState.VISIBLE));
-	    element.click();
-	    extentTest.get().log(Status.INFO, "Clicked on 'Add Appointment' icon.");
-	    System.out.println("Clicked the add icon");
+		String xpath = "//mat-icon[normalize-space()='add']";
+		Locator element = getPage().locator(xpath);
+		element.waitFor(new Locator.WaitForOptions().setState(WaitForSelectorState.VISIBLE));
+		element.click();
+		extentTest.get().log(Status.INFO, "Clicked on 'Add Appointment' icon.");
+		System.out.println("Clicked the add icon");
 	}
-	
-	public  void selectDropdownByFieldNameAndValue(String fieldName, String value) {
-		Locator dropdown = getPage().locator("//mat-label[normalize-space(text())='" + fieldName + "']/ancestor::div[contains(@class,'mat-form-field-infix')]/input[@role='combobox']");
-	    click(dropdown, fieldName);
-	    Locator optionToSelect = getPage().locator("//mat-option//span[normalize-space(text())='" + value + "']");
-	    click(optionToSelect,value);
-	    getPage().waitForTimeout(500);   
-	    extentTest.get().log(Status.INFO, "Selected value '" + value + "' for dropdown field '" + fieldName + "'");
+
+	public void selectDropdownByFieldNameAndValue(String fieldName, String value) {
+		Locator dropdown = getPage().locator("//mat-label[normalize-space(text())='" + fieldName
+				+ "']/ancestor::div[contains(@class,'mat-form-field-infix')]/input[@role='combobox']");
+		click(dropdown, fieldName);
+		Locator optionToSelect = getPage().locator("//mat-option//span[normalize-space(text())='" + value + "']");
+		click(optionToSelect, value);
+		getPage().waitForTimeout(500);
+		extentTest.get().log(Status.INFO, "Selected value '" + value + "' for dropdown field '" + fieldName + "'");
 	}
-	
+
 	public void clicksave() {
-	    String xpath = "//span[normalize-space()='Save']";
-	    Locator element = getPage().locator(xpath);
-	    element.click();
+		String xpath = "//span[normalize-space()='Save']";
+		Locator element = getPage().locator(xpath);
+		element.click();
 	}
-	
+
 //	public void clickYes() {
 //	    Locator yesButton = getPage().locator("//a[normalize-space()='Yes' and contains(@class, 'mat-primary')]");
 //	    yesButton.waitFor(new Locator.WaitForOptions().setState(WaitForSelectorState.VISIBLE).setTimeout(10000));
@@ -59,74 +60,73 @@ public class AppointmentPage extends BaseClass {
 //	    try { Thread.sleep(5000); } catch (InterruptedException e) { e.printStackTrace(); }
 //	}
 
+	public int getTotalAppointmentCount1() {
 
-    public int getTotalAppointmentCount1() {
+		Locator pagination = getPage().locator("li.page-item.m-r-5.ng-star-inserted").first();
+		String text = pagination.textContent().trim();
+		String total = text.split("of")[1].trim();
+		return Integer.parseInt(total);
 
-        Locator pagination = getPage().locator("li.page-item.m-r-5.ng-star-inserted").first();
-        String text = pagination.textContent().trim();  
-        String total = text.split("of")[1].trim();
-        return Integer.parseInt(total);
-        
-    }
-    public int getTotalAppointmentCount() {
-
-        Locator pagination = getPage().locator("li.page-item.m-r-5.ng-star-inserted").first();
-        String text = pagination.textContent().trim();  
-        String total = text.split("of")[1].trim();
-        int count = Integer.parseInt(total);
-        extentTest.get().log(Status.INFO, "Total appointment count retrieved: '" + count + "'");
-        return count;
-    }
-
-
-    public void saveAppointmentHandleAllPopups() {
-        getPage().locator("//span[normalize-space()='Save']").click();
-        System.out.println("Clicked Save button.");
-        sleepSeconds(5); 
-        Locator conflictYesButton = getPage().locator("//a[normalize-space()='Yes' and contains(@class, 'mat-primary')]");
-        Locator beyondOfficeHoursYesButton = getPage().locator(
-                "//div[normalize-space(text())='Warning Alert']//following::a[normalize-space()='Yes' and contains(@class,'mat-primary')][1]");
-        boolean popupHandled;
-        do {
-            popupHandled = false;
-                if (conflictYesButton.isVisible(new Locator.IsVisibleOptions().setTimeout(1000))) {
-                System.out.println("Conflict popup detected. Waiting 5 seconds before clicking 'Yes'...");
-                sleepSeconds(5); 
-                conflictYesButton.click();
-                System.out.println("Clicked Yes on conflict popup.");
-                popupHandled = true;
-                sleepSeconds(5); 
-            }
-
-            if (beyondOfficeHoursYesButton.isVisible(new Locator.IsVisibleOptions().setTimeout(1000))) {
-                System.out.println("Beyond office hours popup detected. Waiting 5 seconds before clicking 'Yes'...");
-                sleepSeconds(5); 
-                beyondOfficeHoursYesButton.click();
-                System.out.println("Clicked Yes on beyond office hours popup.");
-                popupHandled = true;
-                sleepSeconds(5); 
-            }
-
-        } while (popupHandled);
-        sleepSeconds(5);
-        extentTest.get().log(Status.INFO, "All popups handled. Appointment saved successfully.");
-        System.out.println("All popups handled. Appointment saved successfully.");
-    }
-    
-    public void navigateToDashboard() {
-	    Locator icon = getPage().locator("//i[contains(@class,'fa-home')]");
-	    icon.waitFor(new Locator.WaitForOptions().setState(WaitForSelectorState.VISIBLE));
-	    icon.click();
-	    extentTest.get().log(Status.INFO, "Clicked on daashord -> Navigated sucessfully.");
 	}
-    
-    private void sleepSeconds(int seconds) {
-        try {
-            Thread.sleep(seconds * 1000L);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        
-        
-    }
+
+	public int getTotalAppointmentCount() {
+
+		Locator pagination = getPage().locator("li.page-item.m-r-5.ng-star-inserted").first();
+		String text = pagination.textContent().trim();
+		String total = text.split("of")[1].trim();
+		int count = Integer.parseInt(total);
+		extentTest.get().log(Status.INFO, "Total appointment count retrieved: '" + count + "'");
+		return count;
+	}
+
+	public void saveAppointmentHandleAllPopups() {
+		getPage().locator("//span[normalize-space()='Save']").click();
+		System.out.println("Clicked Save button.");
+		sleepSeconds(5);
+		Locator conflictYesButton = getPage()
+				.locator("//a[normalize-space()='Yes' and contains(@class, 'mat-primary')]");
+		Locator beyondOfficeHoursYesButton = getPage().locator(
+				"//div[normalize-space(text())='Warning Alert']//following::a[normalize-space()='Yes' and contains(@class,'mat-primary')][1]");
+		boolean popupHandled;
+		do {
+			popupHandled = false;
+			if (conflictYesButton.isVisible(new Locator.IsVisibleOptions().setTimeout(1000))) {
+				System.out.println("Conflict popup detected. Waiting 5 seconds before clicking 'Yes'...");
+				sleepSeconds(5);
+				conflictYesButton.click();
+				System.out.println("Clicked Yes on conflict popup.");
+				popupHandled = true;
+				sleepSeconds(5);
+			}
+
+			if (beyondOfficeHoursYesButton.isVisible(new Locator.IsVisibleOptions().setTimeout(1000))) {
+				System.out.println("Beyond office hours popup detected. Waiting 5 seconds before clicking 'Yes'...");
+				sleepSeconds(5);
+				beyondOfficeHoursYesButton.click();
+				System.out.println("Clicked Yes on beyond office hours popup.");
+				popupHandled = true;
+				sleepSeconds(5);
+			}
+
+		} while (popupHandled);
+		sleepSeconds(5);
+		extentTest.get().log(Status.INFO, "All popups handled. Appointment saved successfully.");
+		System.out.println("All popups handled. Appointment saved successfully.");
+	}
+
+	public void navigateToDashboard() {
+		Locator icon = getPage().locator("//i[contains(@class,'fa-home')]");
+		icon.waitFor(new Locator.WaitForOptions().setState(WaitForSelectorState.VISIBLE));
+		icon.click();
+		extentTest.get().log(Status.INFO, "Clicked on daashord -> Navigated sucessfully.");
+	}
+
+	private void sleepSeconds(int seconds) {
+		try {
+			Thread.sleep(seconds * 1000L);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+	}
 }
--- src/test/java/smoke_test_cases/AppointmentPageSmokeTest.java
+++ src/test/java/smoke_test_cases/AppointmentPageSmokeTest.java
@@ -7,6 +7,7 @@ import org.framework.playwright.listener.RetryListener;
 import org.framework.playwright.utils.DataFaker;
 import org.playwright.dataproviderclass.DataProviderClasses;
 import org.playwright.modules.AppointmentModule;
+import org.playwright.modules.Ecountermodule;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
@@ -18,37 +19,43 @@ public class AppointmentPageSmokeTest extends CommonFunctions {
 
 	@BeforeClass(alwaysRun = true)
 	public void launchApplication() {
-	    CommonFunctions common = new CommonFunctions();
+		CommonFunctions common = new CommonFunctions();
 
-	    String url = common.getUrl();
-	    String orgCode = prop.getProperty("Orgcode");
-	    String username = prop.getProperty("Username");
-	    String password = prop.getProperty("Password");
-	    common.launchApplication(url, orgCode, username, password);
+		String url = common.getUrl();
+		String orgCode = prop.getProperty("Orgcode");
+		String username = prop.getProperty("Username");
+		String password = prop.getProperty("Password");
+		common.launchApplication(url, orgCode, username, password);
 	}
 
-	
-	@Test(enabled = true,groups = { "Smoke" },retryAnalyzer = RetryListener.class)
-	    public void appointmentCreationTest() {
-	    AppointmentModule appointment = new AppointmentModule(null, getPage());
-	    appointment.appointmentcreation();
+	@Test(enabled = true, groups = { "Smoke" }, retryAnalyzer = RetryListener.class)
+	public void appointmentCreationTest() {
+		AppointmentModule appointment = new AppointmentModule(null, getPage());
+		appointment.appointmentcreation();
 	}
-	
 
-	@Test(enabled = true,groups = "Smoke",dataProviderClass = DataProviderClasses.class, dataProvider = "getSmokeTestCasesData",retryAnalyzer = RetryListener.class)
-	    public void appointmentCheckoutTest(Map<String, Object> testData) {
-	    AppointmentModule appointment = new AppointmentModule(testData, getPage());
-	    appointment.appointmentcheckout();
+	@Test(enabled = true, groups = "Smoke", dataProviderClass = DataProviderClasses.class, dataProvider = "getSmokeTestCasesData", retryAnalyzer = RetryListener.class)
+	public void appointmentCheckoutTest(Map<String, Object> testData) {
+		AppointmentModule appointment = new AppointmentModule(testData, getPage());
+		appointment.appointmentcheckout();
+	}
+
+	@Test(enabled = true, groups = "Sanity", dataProviderClass = DataProviderClasses.class, dataProvider = "getSmokeTestCasesData", retryAnalyzer = RetryListener.class)
+	public void scheduleTileTest(Map<String, Object> testData) {
+		AppointmentModule schedule = new AppointmentModule(testData, getPage());
+		schedule.validateSchedule();
+	}
+
+	@Test(enabled = true, groups = "Sanity", dataProviderClass = DataProviderClasses.class, dataProvider = "getSmokeTestCasesData", retryAnalyzer = RetryListener.class)
+	public void checkedInTileTest(Map<String, Object> testData) {
+		AppointmentModule checkedin = new AppointmentModule(testData, getPage());
+		checkedin.validatecheckedin();
+	}
+
+	@Test(enabled = true, groups = "Sanity", dataProviderClass = DataProviderClasses.class, dataProvider = "getSmokeTestCasesData", retryAnalyzer = RetryListener.class)
+	public void ecounterFormTest(Map<String, Object> testData) {
+		Ecountermodule Encounterform = new Ecountermodule(testData, getPage());
+		Encounterform.validateencounterform();
 	}
 	
-	@Test(enabled = true,groups = "Sanity",dataProviderClass = DataProviderClasses.class, dataProvider = "getSmokeTestCasesData",retryAnalyzer = RetryListener.class)
-    public void scheduleTileTest(Map<String, Object> testData) {
-    AppointmentModule schedule = new AppointmentModule(testData, getPage());
-    schedule.validateSchedule();
-}
-	@Test(enabled = true,groups = "Sanity",dataProviderClass = DataProviderClasses.class, dataProvider = "getSmokeTestCasesData",retryAnalyzer = RetryListener.class)
-    public void checkedInTileTest(Map<String, Object> testData) {
-    AppointmentModule checkedin = new AppointmentModule(testData, getPage());
-    checkedin.validatecheckedin();
-}
 }
